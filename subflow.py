a75241d6c3b4a0423e025dc1c3676cf43a61b88b
managepy-datalouna <managepy.datalouna@protonmail.com>
2024-03-08 17:34:11 +0100

    ADD: a subflow memory leak

---
+++ subflow.py
@@ -0,0 +1,28 @@
+import time
+
+from prefect import flow, get_run_logger
+
+
+def get_data():
+    time.sleep(5)
+    return list(range(1000000))
+
+
+@flow
+def subflow():
+    list_ = get_data() # memory leak is here
+    raise ValueError
+
+
+@flow
+def pipeline():
+    logger = get_run_logger()
+
+    for iteration in range(100):
+        subflow(return_state=True)
+        logger.info(f'{iteration=}')
+
+
+if __name__ == '__main__':
+    pipeline()
+
